0da747d72be3686b3b13c207d66ef8c44313a349
Lucas Storino <storino@poli.ufrj.br>
2022-07-01 20:56:42 -0300

    GET endpoint implementation

--- p3/main.py
+++ p3/main.py
@@ -1,28 +1,31 @@
-from flask import Flask, jsonify, request
+from flask    import Flask, jsonify, request
+from requests import get
 from datetime import date
-
-def to_datetime(date_string):
-    date_list = date_string.split("-")
-    date_list = list(map(lambda x: int(x), date_list))
-    a,m,d = date_list
-    return date(a,m,d)
-
-def format_date(date_string):
-    date_list = date_string.split("-")
-    a,m,d = date_list
-    return f"{d}/{m}/{a}"
 
 app = Flask(__name__)
 
+#POST endpoint implmentation
 @app.route('/age', methods=['POST'])
-def age_post():
+def age():
+
+    def to_datetime(date_string) -> date:
+        date_list = date_string.split("-")
+        date_list = list(map(lambda x: int(x), date_list))
+        a,m,d = date_list
+        return date(a,m,d)
+
+    def format_date(date_string) -> str:
+        date_list = date_string.split("-")
+        a,m,d = date_list
+        return f"{d}/{m}/{a}"
+
     data = dict(request.get_json())
     name, birth, dateThen = [value for value in data.values()]
 
-    birth_datetime = to_datetime(birth)
+    birth_datetime    = to_datetime(birth)
     dateThen_datetime = to_datetime(dateThen)
 
-    ageNow = (date.today() - birth_datetime).days // 365
+    ageNow  = (date.today() - birth_datetime).days // 365
     ageThen = (dateThen_datetime - birth_datetime).days // 365
 
     dateThen = format_date(dateThen)
@@ -35,6 +38,49 @@ def age_post():
         "ageThen": ageThen
     }
 
+    return jsonify(response)
+
+#GET endpoint implementation
+@app.get('/album-info')
+def album_info():
+
+    def get_latest_album(Albums) -> tuple:
+        album_year = 0
+        latest_album = ''
+
+        for album in Albums:
+            if int(album['intYearReleased']) > album_year:
+                album_year   = int(album['intYearReleased'])
+                latest_album = album
+
+        return album_year, latest_album
+
+    artist_name = request.args.get('artist')
+
+    artist_info = get(f'http://theaudiodb.com/api/v1/json/2/search.php?s={artist_name}').json()
+    artist_id   = artist_info['artists'][0]['idArtist']
+    artist_name = artist_info['artists'][0]['strArtist']
+
+    albums_info = get(f'http://theaudiodb.com/api/v1/json/2/album.php?i={artist_id}').json()
+
+    album_year, latest_album  = get_latest_album(albums_info['album'])
+
+    tracks_info = get(f'http://theaudiodb.com/api/v1/json/2/track.php?m={latest_album["idAlbum"]}').json()
+    track1 = tracks_info['track'][0]['strTrack']
+    track2 = tracks_info['track'][1]['strTrack']
+    track3 = tracks_info['track'][2]['strTrack']
+
+    response = {
+        "artist": artist_name,
+        "latest_album": latest_album['strAlbum'],
+        "album_year": album_year,
+        "album_tracks": {
+            1: track1,
+            2: track2,
+            3: track3
+        }
+    }
+    
     return jsonify(response)
 
 app.run()
